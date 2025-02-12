0e7f2b73d2ad24cdf6c69fe4da8caec74e1fa2f5
Adarsh <isadarsh02@gmail.com>
2025-02-12 22:32:47 +0530

    Add initial page components and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Routes, Route } from "react-router-dom";//singular for specific assignment
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage"
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import LogoutPage from "./pages/LogoutPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 export default function App() {
   return (
-    <h1 className="text-red-500">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar />
+      <Routes>//auth-pages
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignupPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/logout" element={<LogoutPage/>} />
+        <Route path="/settings" element={<SettingsPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+      </Routes>
+    </div>
   )
 }
