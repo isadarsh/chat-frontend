926d635555a5e8d6b201a5730ae1a251f85f4055
Adarsh <isadarsh02@gmail.com>
2025-02-13 22:38:55 +0530

    Add authentication functionality and signup page with loader

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,10 +1,134 @@
-  
-import React from 'react'
+import { useState } from "react"
+import { useAuthStore } from "../store/useAuthStore";
+import { Mail, MessageSquare, User, Lock, EyeOff, Eye, Loader2 } from "lucide-react";
+import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
 
 const SignupPage = () => {
-  return (
-    <div>SignupPage</div>
-  )
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const {signUp, isSigningUp} = useAuthStore();
+
+  const validateForm = () => { };
+  const handleSubmit = async (e) => {
+    e.preventDefault();//to not refresh the page
+  };
+  return <div className="min-h-screen grid lg:grid-cols-2">
+    {/* left side screen */}
+    <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+      <div className="w-full max-w-md space-y-8">
+        {/* Logo */}
+        <div className="text-center mb-8">
+          <div className="flex flex-col items-center gap-2 group">
+            <div
+              className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+              group-hover:bg-primary/20 transition-colors">
+              <MessageSquare className="size-6 text-primary" />
+            </div>
+            <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+            <p className="text-base-content/60">Get Started with your free account</p>
+          </div>
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Your Name</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <User className="size-5 text-base-content" />
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="name"
+                  value={formData.name}
+                  onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="size-5 text-base-content" />
+                </div>
+                <input
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@email.com"
+                  value={formData.email}
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="size-5 text-base-content" />
+                </div>
+                <input
+                  type={showPassword? "text": "password"}
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="* * * * * * * *"
+                  value={formData.password}
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <button type="submit" className="btn btn-primary w-full" disabled={isSigningUp}>
+              {isSigningUp ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                </>
+              ) : (
+                  "Create Account"
+              )}
+
+            </button>
+          </form>
+
+          <div className="text-center">
+            <p className="text-base-content/60">
+              Already have an account?{" "}
+              <Link to="/login" className="link link-primary">
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* Right side of page */}
+    <AuthImagePattern
+      title="Join our Community"
+      subtitle="Connect, conversate and collaborate with others"
+
+    />
+  </div>
 }
 
 export default SignupPage
